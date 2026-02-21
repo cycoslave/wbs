cafa4e1dd7bf19615e3a9bdd7bebeec637543739
cyco <tonyroy@wcksoft.com>
2026-02-21 14:40:53 -0400

    brought help system from wbs 5.2.11

--- src/commands.py
+++ src/commands.py
@@ -202,18 +202,265 @@ async def cmd_passwd(core, handle: str, session_id: int, arg: str, respond):
 
 async def cmd_help(core, handle, session_id, arg, respond):
     """Show help"""
-    help_text = """
-WBS Partyline Commands:
-.help      - This help
-.uptime    - Bot uptime
-.version    - Bot version  
-.say #chan msg - Send message
-.msg nick msg - Send message
-.join #chan - Join channel
-.part #chan - Join channel
-.quit      - Shutdown bot
+    # Extract the command (second word)
+    words = arg.split()
+    if len(words) < 1:
+        help_text = """
+.: Wicked Bot System Help :.
+   For all users:
+      help
+      date         time         uptime       version
+      who          quit         whom         chpass
+      handle       whoami       -host        note         
+   For ops:
+      mode         say          msg          op
+      deop         voice        devoice
+   For admins:
+      chattr       backup       status       die
+      modules      +user        +ignore      ignores      
+      -user        -ignore      dccstat      restart
+      +bot         botattr      chhandle     relay
+      +host        -bot         link         chaddr
+      unlink       dccstat      update       channels
+      mnote        bots         join         part
+      lock         unlock       topiclock    topicunlock
+      sdns         swhois       swhowas      links 
+      taskset      timers       tasks        botinfo 
+      nopass       fixpass      mass         net
+      baway        bback        nick         lag 
+      infoleaf     addleaf      addhub       subnet  
+
+All commands begin with '.', and all else goes to the party line.      
 """
-    
+        for line in help_text.split('\n'):
+            await respond(line)
+        return
+
+    cmd = words[0].lower()
+    if cmd == "date":
+        help_text = """
+###  date
+    Shows the current date and time.
+
+See also: time
+"""
+
+    elif cmd == "time":
+        help_text = """
+###  time
+    Shows the current date and time.
+
+See also: date
+"""
+
+    elif cmd == "uptime":
+        help_text = """
+###  uptime
+    Shows the uptime of the bot.
+"""
+
+    elif cmd == "version":
+        help_text = """
+###  version
+    Shows the current version of the bot system.
+"""
+
+    elif cmd == "mode":
+        help_text = """
+###  mode <channel> <arguments>
+    Sets mode on a channel.
+"""
+
+    elif cmd == "mnote":
+        help_text = """
+###  mnote <flag> \\[channel\\] <message>
+    Sends a private note to users with a certain flag on the party line.
+
+See also: note, notes
+"""
+
+    elif cmd == "bots":
+        help_text = """
+###  bots
+    Shows botnet information.
+"""
+
+    elif cmd == "lock":
+        help_text = """
+###  lock <channel> \\[reason\\]
+    Locks a channel.
+
+See also: unlock
+"""
+
+    elif cmd == "unlock":
+        help_text = """
+###  unlock <channel>
+    Unlocks a channel.
+
+See also: lock
+"""
+
+    elif cmd == "topiclock":
+        help_text = """
+###  topiclock <channel> \\[topic\\]
+    Locks the topic of a channel.
+"""
+
+    elif cmd == "sdns":
+        help_text = """
+###  sdns <ip/host>
+    Performs dns resolution on the bot's server.
+"""
+
+    elif cmd == "swhois":
+        help_text = """
+###  swhois <nickname>
+    Performs whois on the bot's server.
+"""
+
+    elif cmd == "swhowas":
+        help_text = """
+###  swhowas <nickname>
+    Performs whowas on the bot's server.
+"""
+
+    elif cmd == "links":
+        help_text = """
+###  links
+    Shows all the servers linked to the network.
+"""
+
+    elif cmd == "taskset":
+        help_text = """
+###  taskset <task> <0/1>
+    Configures tasks to enable or disable them.
+"""
+
+    elif cmd == "timers":
+        help_text = """
+###  timers
+    Shows all the timers on the bot.
+"""
+
+    elif cmd == "tasks":
+        help_text = """
+###  tasks
+    Shows all the tasks configured.
+"""
+
+    elif cmd == "botinfo":
+        help_text = """
+###  botinfo
+    Shows bot information.
+"""
+
+    elif cmd == "nopass":
+        help_text = """
+###  nopass
+    Shows all the users without a password.
+
+See also: fixpass
+"""
+
+    elif cmd == "fixpass":
+        help_text = """
+###  fixpass
+    Sets random passwords to all users without one.
+
+See also: nopass
+"""
+
+    elif cmd == "mass":
+        help_text = """
+###  mass <command> \\[arguments\\]
+    Does mass commands.
+    Valid commands are: op deop
+"""
+
+    elif cmd == "net":
+        help_text = """
+###  net <channel> \\[topic\\]
+    Does commands at the botnet level.
+    Valid commands are: op deop save rehash restart chanset die chanfix chanset mode join part msg
+"""
+
+    elif cmd == "baway":
+        help_text = """
+###  baway \\[reason\\]
+    Puts the bot in away mode.
+
+See also: bback
+"""
+
+    elif cmd == "bback":
+        help_text = """
+###  bback
+    Brings the bot back from away mode.
+
+See also: baway
+"""
+
+    elif cmd == "nick":
+        help_text = """
+###  nick \\[nick\\]
+    Configures the bot's nickname.
+"""
+
+    elif cmd == "lag":
+        help_text = """
+###  lag
+    Shows the botnet latency.
+"""
+
+    elif cmd == "infoleaf":
+        help_text = """
+###  infoleaf
+    Gives the command to add this bot as a leaf on the hub.
+
+See also: addleaf, addhub
+"""
+
+    elif cmd == "addleaf":
+        help_text = """
+###  addleaf <botnick> <host> <port>
+    Adds a leaf bot to the botnet's hub, then gives the command to add the hub.
+
+See also: infoleaf, addhub
+"""
+
+    elif cmd == "addhub":
+        help_text = """
+###  addhub <botnick> <host> <port>
+    Adds the botnet's hub on a botnet leaf.
+
+See also: infoleaf, addleaf
+"""
+
+    elif cmd == "subnet":
+        help_text = """
+###  subnet <command> \\[arguments\\]
+    Configures the bot's subnet.
+    Valid commands are: set list help
+"""
+
+    elif cmd == "update":
+        help_text = """
+###  update
+    Launches the Wicked Bot System update process.
+"""
+
+    elif cmd == "channels":
+        help_text = """
+###  channels
+    Lists all channels.
+"""
+
+    else:
+        help_text = f"""
+ERROR: Unknown command: {text}
+"""
+
     for line in help_text.split('\n'):
         await respond(line)
 
