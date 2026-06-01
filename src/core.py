2a4c9ca5f766a21601aa1db93829ec2cec94313b
Tony Roy <tonyroy@wcksoft.com>
2026-06-01 20:33:22 -0300

    did more changes to core to try to fix the bot linking issues

--- src/core.py
+++ src/core.py
@@ -252,8 +252,9 @@ class Core:
             )
             bot_session.peer_ip = event.get('peer_ip', 'unknown')
             self.bot_sessions[bot_name.lower()] = bot_session
-            asyncio.create_task(self.botnet.read_peer(bot_name, reader, writer), name=f"read_peer:{bot_name}")
+            
             await self.botnet.process_incoming(bot_name, event['data'], reader, writer)
+            asyncio.create_task(self.botnet.read_peer(bot_name, reader, writer), name=f"read_peer:{bot_name}")
             log.debug(f"Bot session {bot_id} created for {bot_name}")
 
         except Exception as e:
